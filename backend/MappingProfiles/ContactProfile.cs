15fc5fabb048b996e282a2822f787777b15cfbe0
Vincent Nadal <vnadal@fullscale.io>
2023-02-13 16:24:14 +0800

    Add mapping profiles

---
+++ backend/MappingProfiles/ContactProfile.cs
@@ -0,0 +1,18 @@
+﻿using API.DTOs;
+using API.Models;
+using AutoMapper;
+
+namespace API.MappingProfiles
+{
+    public class ContactProfile : Profile
+    {
+        // Create a mapping profile for Contact, and for CreateContactDto to generate an Id of type Guid
+        public ContactProfile()
+        {
+            CreateMap<Contact, ReadContactDTO>().ReverseMap();
+            CreateMap<CreateContactDTO, Contact>()
+                .ForMember(c => c.Id, opt => opt.MapFrom(src => Guid.NewGuid())).ReverseMap();
+            CreateMap<UpdateContactDTO, Contact>().ReverseMap();
+        }
+    }
+}
