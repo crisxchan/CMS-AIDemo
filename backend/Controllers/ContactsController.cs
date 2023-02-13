299eeca8cdb0965dac7607602f572cd2e6d4055a
Christian Benedict Gonzales <chgonzales@fullscale.io>
2023-02-13 16:41:44 +0800

    Complete backend api

---
+++ backend/Controllers/ContactsController.cs
@@ -0,0 +1,56 @@
+﻿using API.DTOs;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        // CRUD HTTP Requests handler
+
+        // Create a read contact http get
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_contactService.GetContacts());
+        }
+
+        // Create a read contact http get given an ID
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            return Ok(_contactService.GetContact(id));
+        }
+
+        // Create a create contact http post
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateContactDTO contact)
+        {
+            return Ok(_contactService.AddContact(contact));
+        }
+
+        // Create a update contact http put
+        [HttpPut]
+        public IActionResult Put([FromBody] UpdateContactDTO contact)
+        {
+            return Ok(_contactService.UpdateContact(contact));
+        }
+
+        // Create a delete contact
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            _contactService.DeleteContact(id);
+            return NoContent();
+        }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,17 @@
+using API.Data;
+using API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add the repository as scoped
+builder.Services.AddScoped<IContactService, JsonContactService>();
+
+// Add the JsonContactservice as scoped
+builder.Services.AddScoped<JsonRepository>();
+
+// Add the automapper package
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +26,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// frontend react => localhost:3000
+// add a CORS policy that accepts requests from localhost:3000
+app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
 
 app.UseAuthorization();
 
